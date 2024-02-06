a86acb604a3a6e0644977fc2dbd9bebf39e4df4c
DarjaDav0 <darja.davydova.dd@gmail.com>
2024-02-06 19:33:49 +0200

    Timestamp added, code not yet working
    I tried to add timezone to timestamp, like pymongo documentation suggests. Having problem with VSCode. IT doesn't recognize pytz library even after pip install.

--- inserting_data.py
+++ inserting_data.py
@@ -1,21 +1,25 @@
 import asyncio
 import pymongo
 from ruuvitag_sensor.ruuvi import RuuviTagSensor
+from datetime import datetime
+from pytz import timezone
 
 myclient = pymongo.MongoClient("mongodb://localhost:27017")
 mydb = myclient["Ruuvi_tag"]
 mycol = mydb["ruuvi-tags_data"]
 
-# Replace with your RuuviTag sensor's MAC addresses
 macs = ['CE:1B:AC:05:D6:3F']
 
 async def main():
     async for data in RuuviTagSensor.get_data_async(macs):
-        mac_address = data[0]
+        mac_address = data[0] 
         sensor_data = data[1]
 
-        # Add the MAC address to the sensor data
         sensor_data["CE:1B:AC:05:D6:3F"] = mac_address
+
+        # Add the current timestamp and timezone to the sensor data
+        sensor_data["timestamp"] = datetime.now()
+        sensor_data["timezone"] = datetime.now(pytz.timezone('Europe/Helsinki')).strftime('%Z%z')
 
         # Insert the sensor data into the MongoDB collection
         result = mycol.insert_one(sensor_data)
